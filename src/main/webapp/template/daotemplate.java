a7c3f251eda3f41fd176d2e6125899779e4d1a18
245655812@qq.com <a19921121>
2016-06-30 23:25:07 +0800

    加注释

--- src/main/webapp/template/daotemplate.java
+++ src/main/webapp/template/daotemplate.java
@@ -2,6 +2,13 @@
 
 import org.springframework.stereotype.Repository;
 
+/**
+ * 开发者
+ * nickName:大黄蜂
+ * email:245655812@qq.com
+ * github:https://github.com/bigbeef
+ * velocity模板生成 cppba-codeTemplate
+ */
 @Repository
 public class $!{domainName}DAO extends BaseDao{
 
